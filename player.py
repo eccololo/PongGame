a17912a9d56ed6893f7691a33c239e9f49f757f2
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-05-17 19:44:01 +0200

    Creating a player1 and player2 objects in main game file and adding starting position of players constants.

--- player.py
+++ player.py
@@ -1,8 +1,7 @@
 from turtle import Turtle
 
-
 class Player(Turtle):
 
-    def __init__(self):
+    def __init__(self, player_pos_tuple):
         super().__init__()
         pass
--- pong_game.py
+++ pong_game.py
@@ -7,11 +7,18 @@
 
 import turtle
 from turtle import Screen
+from player import Player
+
+PLAYER_1_START_POSITION = (300, 0)
+PLAYER_2_START_POSITION = (-300, 0)
 
 screen = Screen()
 screen.setup(width=900, height=550)
 screen.bgcolor("black")
 screen.tracer(0)
+
+player1 = Player(PLAYER_1_START_POSITION)
+player2 = Player(PLAYER_2_START_POSITION)
 
 game_is_on = True
 
