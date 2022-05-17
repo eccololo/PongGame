795f46cc055e9f8e1af0dc5f1344c6ff253c3014
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-05-17 19:33:16 +0200

    second commit

---
+++ pong_game.py
@@ -0,0 +1,7 @@
+# TODO:
+#    1. Create class paddle or player and make it move and bounce a ball.
+#    2. Create class ball and make it move and bounce from walls and players.
+#    3. Create screen.
+#    4. Create class scoreboard and display score.
+#    5. Create vertical line on a middle of the screen.
+#    6. Create game end when ball move out of window.
