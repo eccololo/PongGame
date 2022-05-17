d6f388d1cfafc6c30cea87f51304534f7bdc9dba
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-05-17 19:38:15 +0200

    Creating main game loop.

--- pong_game.py
+++ pong_game.py
@@ -1,7 +1,6 @@
 # TODO:
 #    1. Create class paddle or player and make it move and bounce a ball.
 #    2. Create class ball and make it move and bounce from walls and players.
-#    3. Create screen.
 #    4. Create class scoreboard and display score.
 #    5. Create vertical line on a middle of the screen.
 #    6. Create game end when ball move out of window.
@@ -13,6 +12,11 @@ screen = Screen()
 screen.setup(width=900, height=550)
 screen.bgcolor("black")
 screen.tracer(0)
+
+game_is_on = True
+
+while game_is_on:
+    pass
 
 
 screen.exitonclick()
