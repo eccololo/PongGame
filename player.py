adbb6052ae0fc1ab9c8a855eb01a6e9eeba19e21
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-06-09 18:31:04 +0200

    Reorganizing player.py class file.

--- player.py
+++ player.py
@@ -3,13 +3,6 @@ from turtle import Turtle
 
 class Player(Turtle):
 
-    def __init__(self, player_pos_tuple):
+    def __init__(self):
         super().__init__()
-        self.color("white")
-        self.shape("turtle")
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.speed("fastest")
-        self.setx(10)
-        self.sety(10)
-        self.penup()
-        self.goto(self.xcor(), self.ycor())
+
