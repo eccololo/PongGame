154fbfca537bf3b0f3a61a969bb8960482de15d2
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-06-09 18:39:15 +0200

    Writing initial code in init Player class that player is visible on screen.

--- player.py
+++ player.py
@@ -6,7 +6,8 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
-        self.hideturtle()
-        self.shapesize(stretch_wid=0.5, stretch_len=1.5)
-        self.goto(390, 0)
+        self.shape("square")
+        self.shapesize(stretch_wid=6, stretch_len=1)
+        self.penup()
+        self.goto(190, 0)
 
