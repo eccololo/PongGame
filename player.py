ab68004ce87c7e17fe524002023e38c648a62857
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-06-09 18:42:05 +0200

    Positioning right player paddle.

--- player.py
+++ player.py
@@ -3,11 +3,11 @@ from turtle import Turtle
 
 class Player(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.color("white")
         self.shape("square")
         self.shapesize(stretch_wid=6, stretch_len=1)
         self.penup()
-        self.goto(190, 0)
+        self.goto(position[0], position[1])
 
