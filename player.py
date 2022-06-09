a1bd375a628ec4deae7fc9deb65f50ffa24b805c
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-06-09 18:35:29 +0200

    Writing initial code in Player init method.

--- player.py
+++ player.py
@@ -5,4 +5,8 @@ class Player(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.shapesize(stretch_wid=0.5, stretch_len=1.5)
+        self.goto(390, 0)
 
