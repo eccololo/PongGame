e7abba41602d70b3128f88403dc5c15deaed21da
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-06-09 18:47:09 +0200

    Making right paddle move.

--- pong_game.py
+++ pong_game.py
@@ -17,8 +17,8 @@ right_player = Player((390, 0))
 left_player = Player((-390, 0))
 
 screen.listen()
-screen.onkey(right_player.right_player_move_up, "up")
-screen.onkey(right_player.right_player_move_down, "down")
+screen.onkey(right_player.right_player_move_up, "Up")
+screen.onkey(right_player.right_player_move_down, "Down")
 
 game_is_on = True
 
