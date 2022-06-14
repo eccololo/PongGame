7d5d869b2ada087e4c199f9d77843bf01e5b546b
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-06-14 19:02:15 +0200

    Writing initial score to game screen.

--- scoreboard.py
+++ scoreboard.py
@@ -5,8 +5,12 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.r_score = 0
+        self.l_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
-        self.goto(100, 100)
-        self.write("Hello World", align="center", font=("Verdana", 15, "normal"))
+        self.goto(150, 200)
+        self.write(self.r_score, align="center", font=("Verdana", 45, "normal"))
+        self.goto(-150, 200)
+        self.write(self.l_score, align="center", font=("Verdana", 45, "normal"))
